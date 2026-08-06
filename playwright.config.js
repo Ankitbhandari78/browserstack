49e3d09da42553c22602d0ba4253671b35e019f3
Rene Sebera <renesebera@192.168.1.3>
2026-08-06 21:55:57 +0530

    Run tests only on Google Chrome

--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,10 @@
 // playwright.config.js
 const { defineConfig } = require('@playwright/test');
 
-const isCI = !!process.env.CI;
-
 module.exports = defineConfig({
   testDir: './e2e',   // tells Playwright to look inside e2e folder
   use: {
-    // On CI: use Playwright's bundled Chromium in headless mode
-    // Locally: use installed Google Chrome in headed mode
-    channel: isCI ? undefined : 'chrome',
-    headless: isCI,
+    channel: 'chrome', // run in installed Google Chrome
+    headless: false,   // show browser window
   },
 });
